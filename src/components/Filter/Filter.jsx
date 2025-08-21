3fe125cf49b8029fab59f02de4c341b98a2881bc
BilalFirtina <b.firtana@gmail.com>
2025-08-21 16:13:41 +0300

    correction

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { changeFilter } from "../../redux/filters/slice";
-import { selectFilter } from "../../redux/filters/selectors";
+import { selectFilter } from "../../redux/filters/selector";
 
 const Filter = () => {
   const dispatch = useDispatch();
